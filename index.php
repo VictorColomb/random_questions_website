8a619ef24741a123ebf0eb5a74bba92910d4883c
Victor Colomb <vic.col@hotmail.fr>
2020-04-21 15:34:23 +0200

    Google analytics

--- index.php
+++ index.php
@@ -31,6 +31,16 @@
 
     <!--Script-->
     <script src="scripts.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164106797-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-164106797-1');
+    </script>
+
 </head>
 
 <body onload='on_load();' onkeydown="keyDown(event.keyCode);">
